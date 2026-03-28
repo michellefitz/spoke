85e6e7b84fb1353417ca404eae2e00c31c826259
Michelle Fitzpatrick <michelle@intercom.io>
2026-03-28 16:49:44 +0000

    feat: add tag field to SpokeTask model

--- Spoke/Models/Task.swift
+++ Spoke/Models/Task.swift
@@ -9,13 +9,17 @@ class SpokeTask {
     var isCompleted: Bool
     var completedAt: Date?
     var createdAt: Date
+    var deadline: Date?
+    var tag: String?
 
-    init(title: String, taskDescription: String? = nil) {
+    init(title: String, taskDescription: String? = nil, deadline: Date? = nil, tag: String? = nil) {
         self.id = UUID()
         self.title = title
         self.taskDescription = taskDescription
         self.isCompleted = false
         self.completedAt = nil
         self.createdAt = .now
+        self.deadline = deadline
+        self.tag = tag
     }
 }
